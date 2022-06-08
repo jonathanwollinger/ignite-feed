2a1441467820c138796b911409304375c8c00e09
Jonathan Wollinger <jonathanwollinger@gmail.com>
2022-06-08 01:36:35 -0300

    component/ create Sidebar Profile and render style

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { Header } from './components/Header';
 
 import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header/>
       <div className={styles.wrapper}>
-        <aside>aside</aside>
+        <Sidebar />
         <main>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore asperiores eaque impedit explicabo temporibus, error magnam qui, eos veritatis voluptates accusantium at repellendus est alias ut consectetur officiis sint incidunt?
         </main>
