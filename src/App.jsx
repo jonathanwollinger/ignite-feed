ee2dfe1f8cc273c87e2e68e780f749900205bede
Jonathan Wollinger <jonathanwollinger@gmail.com>
2022-06-08 00:26:35 -0300

    struct/ created page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { Header } from './components/Header';
+
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
   return (
-    <Header/>
+    <div>
+      <Header/>
+      <div className={styles.wrapper}>
+        <aside>aside</aside>
+        <main>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore asperiores eaque impedit explicabo temporibus, error magnam qui, eos veritatis voluptates accusantium at repellendus est alias ut consectetur officiis sint incidunt?
+        </main>
+      </div>
+    </div>
   )
 }
